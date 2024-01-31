20e415df1e91234c58fdd7e0cf63d0964b750fd0
TOYINNO <toyinnomons@gmail.com>
2024-01-31 11:40:57 +0100

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Avatar() {
+  return (
+    <div className="avatar">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDiqqyESSz4Pzw5OimCt8TSKS4NOgWku8LuQ&usqp=CAU" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Ashley Christian</h1>
+      <p>
+        A front-end developer and Chemical Engineering graduate from FUPRE,
+        currently serving as a Research Assistant. Passionate about crafting
+        user-friendly interfaces and exploring the convergence of technology and
+        science.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill name="HTML+CSS" emoji="💪" />
+      <Skill name="JavaScript" emoji="💪" />
+      <Skill name="Git and Git-Hub" emoji="👍" />
+      <Skill name="React" emoji="💪" />
+      <Skill name="Web Design" emoji="💪" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill">
+      <span>{props.name}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
